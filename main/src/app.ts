d7b7c1340be8059f97f01682649f851b63804f53
amiftachulh <81857387+amiftachulh@users.noreply.github.com>
2023-01-30 22:39:08 +0700

    Add db connection checking middleware

--- main/src/app.ts
+++ main/src/app.ts
@@ -2,13 +2,14 @@ import express, { Express, json } from "express";
 import config from "./config";
 import cors from "cors";
 import { indexRouter } from "./api/index.router";
+import { checkDbConnection } from "./api/middleware";
 
 const app: Express = express();
 
 app.use(cors({ credentials: true, origin: true }));
 app.use(json({ strict: false }));
 
-app.use("/api", indexRouter);
+app.use("/api", checkDbConnection(), indexRouter);
 
 const host = config.server.host;
 const port = config.server.port;
--- task/src/api/middleware.ts
+++ task/src/api/middleware.ts
@@ -1,5 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { AnyZodObject } from "zod";
+import { prisma } from "../db/client";
+
+export function checkDbConnection() {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await prisma.$connect();
+      return next();
+    } catch (error) {
+      console.log("Error: Tidak bisa terhubung ke database");
+      return res.status(503).send({ message: "Service tidak tersedia" });
+    }
+  };
+}
 
 export function validate(schema: AnyZodObject) {
   return async (req: Request, res: Response, next: NextFunction) => {
