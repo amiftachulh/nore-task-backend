a16229a1cd072fa076ba732d0b5264417aeca3a6
A.Miftachul Hidayat <miftachul.mkb@gmail.com>
2023-02-21 13:15:03 +0700

    Update komentar.schema.ts

--- task-service/src/schema/komentar.schema.ts
+++ task-service/src/schema/komentar.schema.ts
@@ -8,7 +8,12 @@ export const komentarCreate = z
   })
   .strict();
 
-export const komentarUpdate = komentarCreate.omit({ taskId: true });
+export const komentarUpdate = z
+  .object({
+    userId: z.string().uuid(),
+    konten: z.string().min(1).max(500),
+  })
+  .strict();
 
 export type KomentarCreate = z.infer<typeof komentarCreate>;
 export type KomentarUpdate = z.infer<typeof komentarUpdate>;
