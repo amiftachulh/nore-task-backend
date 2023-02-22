dc61d16c85b234fd8e383fda985cc9ced0fbb054
A.Miftachul Hidayat <miftachul.mkb@gmail.com>
2023-02-22 11:36:18 +0700

    Update label color regex

--- task-service/src/schema/label-subtask.schema.ts
+++ task-service/src/schema/label-subtask.schema.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const labelSubtaskCreate = z.object({
   nama: z.string().min(1).max(191),
   subtaskId: z.string().uuid(),
-  bgColor: z.string().length(7),
-  color: z.string().length(7),
+  bgColor: z.string().regex(new RegExp("^#([A-Fa-f0-9]{6})$")),
+  color: z.string().regex(new RegExp("^#([A-Fa-f0-9]{6})$")),
 });
 
 export const labelSubtaskUpdate = labelSubtaskCreate.omit({ subtaskId: true });
--- task-service/src/schema/label-task.schema.ts
+++ task-service/src/schema/label-task.schema.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const labelTaskCreate = z.object({
   nama: z.string().min(1).max(191),
   taskId: z.string().uuid(),
-  bgColor: z.string().length(7),
-  color: z.string().length(7),
+  bgColor: z.string().regex(new RegExp("^#([A-Fa-f0-9]{6})$")),
+  color: z.string().regex(new RegExp("^#([A-Fa-f0-9]{6})$")),
 });
 
 export const labelTaskUpdate = labelTaskCreate.omit({ taskId: true });
