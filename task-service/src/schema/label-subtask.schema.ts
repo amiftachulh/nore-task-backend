152adfe42479dacec7c93a4ff4f82f9cdf89f169
A.Miftachul Hidayat <miftachul.mkb@gmail.com>
2023-02-22 11:24:15 +0700

    Update background color and font color for label

--- task-service/src/schema/label-subtask.schema.ts
+++ task-service/src/schema/label-subtask.schema.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const labelSubtaskCreate = z.object({
   nama: z.string().min(1).max(191),
   subtaskId: z.string().uuid(),
+  bgColor: z.string().length(7),
+  color: z.string().length(7),
 });
 
 export const labelSubtaskUpdate = labelSubtaskCreate.omit({ subtaskId: true });
--- task-service/src/schema/label-task.schema.ts
+++ task-service/src/schema/label-task.schema.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const labelTaskCreate = z.object({
   nama: z.string().min(1).max(191),
   taskId: z.string().uuid(),
+  bgColor: z.string().length(7),
+  color: z.string().length(7),
 });
 
 export const labelTaskUpdate = labelTaskCreate.omit({ taskId: true });
